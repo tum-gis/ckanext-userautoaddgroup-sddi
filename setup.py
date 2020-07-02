419e3f98108f372de0fc047fcab21c0ac68fadfd
Mandana Moshref <many.yammy@gmail.com>
2020-07-02 17:37:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # http://packaging.python.org/en/latest/tutorial.html#version
     version='1.0.3',
 
-    description='''CKAN extension to add new users to groups and organisations''',
+    description='''CKAN extension to add new users to groups''',
     long_description=long_description,
 
     # The project's main homepage.
@@ -43,13 +43,12 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ],
 
 
     # What does your project relate to?
-    keywords='''CKAN user organization group''',
+    keywords='''CKAN user group''',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
